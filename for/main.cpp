1e279be1edfa7969184e2746ac70229bca6e58c7
Di@github1 <alrelianodiogo@gmail.com>
2024-05-25 13:41:02 -0300

    atividade de for

---
+++ for/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int x, y, z;
+	
+	for(x=0, y=1, z=2; x<10; x++, y++, z++){
+		cout << x <<" - "<< y <<" - "<< z<< "\n";
+		
+	}
+	return 0;
+}
